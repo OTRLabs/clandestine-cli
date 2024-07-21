45f91ea1aa2f10d32102ffdbc58fc6192578e301
Cam <cammclain@skiff.com>
2024-07-21 23:21:54 +0000

    Refactor and rename db.py to surrealdb_manager.py

--- src/framework/application/database/surrealdb_manager.py
+++ src/framework/application/database/surrealdb_manager.py
@@ -4,4 +4,6 @@ from ..configs.base import Settings
 from surrealdb import AsyncSurrealDB
 
 
-
+class SurrealDBSessionManager:
+    async def __init__(self) -> None:
+        self.db: AsyncSurrealDB = AsyncSurrealDB(f"{Settings.database_settings}")
