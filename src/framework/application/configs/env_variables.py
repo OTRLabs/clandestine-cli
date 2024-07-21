0a625b337044ea3a8c9ac40cfbee38b6b802a8fc
Cam <cammclain@skiff.com>
2024-07-21 08:47:09 +0000

    feat: Add LANGUAGE attribute to BaseSettings class

--- src/framework/application/configs/env_variables.py
+++ src/framework/application/configs/env_variables.py
@@ -11,13 +11,14 @@ class BaseSettings:
     DEBUG: bool = False
     TESTING: bool = False
     VERSION: str = "0.1.0"
+    LANGUAGE: str = "en_US"
 class SecuritySettings:
     SECRET_KEY: str = os.getenv("SECRET_KEY", "secret")
 
 class DatabaseSettings:
     SQLITE_DATABASE_URL: str = os.getenv("SQLITE_DATABASE_URL", "sqlite:///./database.db")
     
-    
+
 class Settings:
     
     base_settings: BaseSettings = BaseSettings()
