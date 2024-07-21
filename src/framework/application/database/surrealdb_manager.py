f547f0703cd36f195870537ca3b8b9eba6f0b5e6
Cam <cammclain@skiff.com>
2024-07-21 23:36:56 +0000

    Refactor and rename db.py to surrealdb_manager.py

--- src/framework/application/database/surrealdb_manager.py
+++ src/framework/application/database/surrealdb_manager.py
@@ -4,6 +4,13 @@ from ..configs.base import Settings
 from surrealdb import AsyncSurrealDB
 
 
-class SurrealDBSessionManager:
+class SurrealDBHTTPSessionManager:
     async def __init__(self) -> None:
-        self.db: AsyncSurrealDB = AsyncSurrealDB(f"{Settings.database_settings}")
+        self.surreal_db_http_connection: AsyncSurrealDB = AsyncSurrealDB(f"{Settings.database_settings.SURREALDB_HTTP_URL}")
+        self.surreal_db_http_connection.set_ssl(Settings.database_settings.SURREALDB_HTTP_SSL)
+        
+
+
+class SurrealDBWSManager:
+    async def __init__(self) -> None:
+        self.surreal_db_websocket_connection: AsyncSurrealDB = AsyncSurrealDB(f"{Settings.database_settings.SURREALDB_WS_URL}")    
