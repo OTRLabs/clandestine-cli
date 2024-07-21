1ac2820929de638933b4e73e3c561c5396714e04
Cam <cammclain@skiff.com>
2024-07-21 08:44:17 +0000

    Update main.py to print the Framework version with the help of the Config class

--- src/framework/main.py
+++ src/framework/main.py
@@ -5,10 +5,17 @@ from typing import TYPE_CHECKING, Any, Dict, List, Optional, Tuple, Union
 from rich.console import Console
 
 
+from .application.configs.base import Config
 
+if TYPE_CHECKING:
+    APP_SETTINGS: Config = Config.get_settings()
 
 
 def main() -> None:
     
     console: Console = Console()
-    console.print(f"Framework v{}", style="bold green")
+    console.print(f"Starting up Framework v{APP_SETTINGS.settings.base_settings.VERSION}", style="bold green")
+    
+    
+if __name__ == "__main__":
+    main()
