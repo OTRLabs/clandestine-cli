631e74e0f98c78bd76c3fe183c2680d8606b853c
Cam <cammclain@skiff.com>
2024-07-21 13:30:33 +0000

    Refactor setup process in main.py

--- src/main.py
+++ src/main.py
@@ -1,19 +1,13 @@
 from __future__ import annotations
-
 from typing import TYPE_CHECKING, Any, Dict, List, Optional, Tuple, Union
-
 from rich.console import Console
-
-
 from framework.application.configs.base import Config
-
 from framework.application.cli.strings.messages.en_US import *
 from framework.application.cli.console_manager import ConsoleManager
 from framework.application import app
+import asyncio
 
-    
-
-def main() -> None:
+async def main() -> None:
     """
     The main function of the app.
     Launches the application and starts the CLI.
@@ -25,17 +19,17 @@ def main() -> None:
     system_settings: app.SetupFramework = app.SetupFramework()
     
     CURRENT_CONSOLE.print(f"{LOADING_MESSAGE}", style="bold blue")
-    system_settings.setup_database()
+    await system_settings.setup_database()
     CURRENT_CONSOLE.print(f"{DATABASE_LOADED_MESSAGE}", style="bold green")
     
     
-    system_settings.setup_cache()
+    await system_settings.setup_cache()
     CURRENT_CONSOLE.print(f"{CACHE_LOADED_MESSAGE}", style="bold green")
     
-    system_settings.setup_task_queue()
+    await system_settings.setup_task_queue()
     CURRENT_CONSOLE.print(f"{TASK_QUEUE_LOADED_MESSAGE}", style="bold green")
     
-    system_settings.setup_logging()
+    await system_settings.setup_logging()
     CURRENT_CONSOLE.print(f"{LOGGING_LOADED_MESSAGE}", style="bold green")
     
     
@@ -45,4 +39,4 @@ def main() -> None:
     ## load all the modules
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
