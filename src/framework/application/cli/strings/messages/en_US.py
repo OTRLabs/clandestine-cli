fc6f3e2ccc35487597a6c8655c933ab97517388d
Cam <cammclain@skiff.com>
2024-07-21 15:04:44 +0000

    Refactor import statements in en_US.py

--- src/framework/application/cli/strings/messages/en_US.py
+++ src/framework/application/cli/strings/messages/en_US.py
@@ -4,7 +4,7 @@ from ....configs.env_variables import Settings
 from ....configs.base import Config
 from ....configs.env_variables import Settings
 from rich import print
-
+from typing import List
     
 # Assuming Config.get_settings is a static or class method returning an instance of Settings.
 app_settings_instance = Config.get_settings(self=Config)
