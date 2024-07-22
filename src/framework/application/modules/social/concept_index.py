b06b371d0019e5bdd231e56cdcaee9398068a28d
Cam <cammclain@skiff.com>
2024-07-22 08:24:28 +0000

    refactor: Add phishing module to SocialEngineeringModules class

--- src/framework/application/modules/social/concept_index.py
+++ src/framework/application/modules/social/concept_index.py
@@ -1,5 +1,13 @@
 from __future__ import annotations
 
 import jinja2
+from rich.console import Console
+from ...cli.strings.messages.en_US import * 
 class SocialEngineeringModules:
+    
     class PhishingModules:
+        async def __init__(self, console: Console):
+            self.console = console
+            self.console.print(PHISHING_MODULES)
+            self.console.print(PhishingModulesOptions)
+            self.console.print()
