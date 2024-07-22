953c506194544396325ec1a8979e54bc28ddc722
Cam <cammclain@skiff.com>
2024-07-22 04:45:25 +0000

    refactor: Update database setup to use console parameter in setup_database and setup_task_queue

--- src/framework/application/app.py
+++ src/framework/application/app.py
@@ -21,14 +21,16 @@ class SetupFramework:
         # Initialize APP_SETTINGS in the constructor
         self.APP_SETTINGS = Config.get_settings(self=Config)
 
-    async def setup_database(self) -> declarative_base:
+    async def setup_database(self, console: Console) -> declarative_base:
         try:
             # Access the SQLITE_DATABASE_URL directly from APP_SETTINGS
             engine = create_engine(self.APP_SETTINGS.database_settings.SQLITE_DATABASE_URL)
+            console.print(f"Setting up database")
             Base = declarative_base(bind=engine)
-            print(f"Created database at {self.APP_SETTINGS.database_settings.SQLITE_DATABASE_URL}")
+            console.print(f"Created database at {self.APP_SETTINGS.database_settings.SURREAL_DB_DATABASE}")
             return Base
         except Exception as e:
+            console.print(f"Error setting up database: {str(e)}")
             logging.error(f"Error setting up database: {str(e)}")
     
     
@@ -39,8 +41,10 @@ class SetupFramework:
     async def setup_module_services(self, console: Console) -> None:
         pass
     
-    async def setup_task_queue(self) -> None:
-        pass
+    async def setup_task_queue(self, console: Console) -> None:
+        console.print("Setting up task queue using SAQ")
+        
+        return None    
     
     async def setup_logging(self) -> None:
         # read the logging level from the settings config
