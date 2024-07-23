ace8e3ccfda39689f19e7a18744b5a7af17ea602
Cam <cammclain@skiff.com>
2024-07-23 04:48:49 +0000

    feat: Convert WindowsCommandExecutor to async
    The `WindowsCommandExecutor` class is converted to an asynchronous implementation to improve performance and allow for non-blocking execution of commands on a Windows system.

--- src/framework/application/modules/payloads/singles/windows/exec.py
+++ src/framework/application/modules/payloads/singles/windows/exec.py
@@ -7,11 +7,11 @@ class WindowsCommandExecutor:
     """
     A class that will execute a command on a Windows system.
     """
-    def __init__(self, console: Console, command: str):
+    async def __init__(self, console: Console, command: str):
         self.command: str = f"{command}"
         console.print(f"Executing command: {command}")
 
-    def execute(self, console: Console, command: str) -> None:
+    async def execute(self, console: Console, command: str) -> None:
         """
         Execute the command on the Windows host. Optimize the command for stealth where possible, using progressively advanced TTPs.
         """
