77f2cab7569c7fe38faf2da328d2f04e328aa1af
Cam <cammclain@skiff.com>
2024-07-24 13:14:17 -0500

    feat: Add recon_managers module
    This commit adds the `recon_managers.py` module to the `src/framework/application/modules/recon` directory. The module imports necessary dependencies and defines classes for managing recon operations. This addition enhances the application's recon functionality and provides a foundation for future development in this area.

---
+++ src/framework/application/modules/recon/recon_managers.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from typing import List, Dict
+
+from rich.console import Console
+from rich.prompt import Prompt
+from ...cli.strings.messages.en_US import *
+from ...cli.command_handler import CommandHandler
