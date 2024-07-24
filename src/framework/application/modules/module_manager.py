5376bd520ea155437631bf0031965179f62f563e
Cam <cammclain@skiff.com>
2024-07-24 05:09:41 +0000

    feat: Initialize ModuleManager class
    This commit initializes the `ModuleManager` class in the `module_manager.py` file. The `__init__` method is added to the class, providing a basic structure for future functionality.

--- src/framework/application/modules/module_manager.py
+++ src/framework/application/modules/module_manager.py
@@ -3,6 +3,14 @@ from __future__ import annotations
 from ..cli.console_manager import ConsoleManager
 
 class ModuleManager:
+    
+    async def __init__(self) -> None:
+        """
+        Initialize the ModuleManager class.
+        
+        """
+        
+        pass
     async def index_module_list(console: ConsoleManager.APPLICATION_CONSOLE) -> dict:
         """
         List all modules in the app.
