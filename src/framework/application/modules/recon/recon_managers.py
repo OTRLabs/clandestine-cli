3fca155b0796ce8beba5c3c15827d0a6e92b4496
Cam <cammclain@skiff.com>
2024-07-24 13:20:40 -0500

    refactor: Improve ReconModuleManagers class
    This commit refactors the `ReconModuleManagers` class in the `recon_managers.py` module. It improves the code structure and readability by organizing the methods and adding docstrings for better documentation. This refactor enhances the maintainability and understandability of the codebase.

--- src/framework/application/modules/recon/recon_managers.py
+++ src/framework/application/modules/recon/recon_managers.py
@@ -6,3 +6,30 @@ from rich.console import Console
 from rich.prompt import Prompt
 from ...cli.strings.messages.en_US import *
 from ...cli.command_handler import CommandHandler
+
+from rich.json import JSON
+
+
+
+class ReconModuleManagers:
+
+    """
+    Managers the recon modules.
+    """
+
+    async def __init__(self) -> None:
+        """
+        Initialize the ReconModuleManagers class.
+        """
+        pass
+
+    async def index_recon_module_list(console: Console) -> dict:
+        """
+        List all recon modules in the app.
+
+        Returns:
+            dict: A dictionary containing all the recon modules.
+        """
+        recon_module_index: dict = {}
+
+        return recon_module_index
