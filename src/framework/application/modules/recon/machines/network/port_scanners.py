1e8054974590b4f5576a7d25e621740a1e7ce32c
Cam <cammclain@skiff.com>
2024-07-24 13:21:29 -0500

    refactor: Update import paths in port_scanners.py
    This commit updates the import paths in the `port_scanners.py` module to reflect the changes in the project's directory structure. The import statements for the `cli.strings.messages.en_US` module and the `cli.command_handler` module are modified to account for the new directory hierarchy. This refactor ensures that the codebase remains functional and up-to-date with the latest project structure.

--- src/framework/application/modules/recon/machines/network/port_scanners.py
+++ src/framework/application/modules/recon/machines/network/port_scanners.py
@@ -4,8 +4,8 @@ from typing import List, Dict
 
 from rich.console import Console
 from rich.prompt import Prompt
-from ...cli.strings.messages.en_US import *
-from ...cli.command_handler import CommandHandler
+from .....cli.strings.messages.en_US import *
+from .....cli.command_handler import CommandHandler
 
 from rich.json import JSON
 
